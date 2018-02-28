3440b52817034645d4d3596f4e5cfc72814f273a
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2018-02-28 21:13:42 +0000

    Adding name and growUp functions and developing constructor function and prototype up til step 5

--- __tests__/virtual-pet-code.test.js
+++ __tests__/virtual-pet-code.test.js
@@ -1,7 +1,23 @@
 const Pet = require ('../virtual-pet-code/virtual-pet')
 
-test('Pet has a name', () => {
-    const pet
-    
-    expect name = name;
+test('Pet has a name', () => {   
+   let fred;
+   fred = new Pet('fred');
+   expect(fred.name).toEqual('fred');
+});
+
+/**test('growUp', () => {
+    it('increments the age by 1',() => {
+        let fred;
+        fred = new Pet('fred');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    });
+});**/  
+
+test('growUp increments the age by 1', () => {
+        let fred;
+        fred = new Pet('fred');
+        fred.growUp();
+        expect(fred.age).toEqual(1);
 });
--- virtual-pet-code/virtual-pet.js
+++ virtual-pet-code/virtual-pet.js
@@ -1,25 +1,36 @@
-/*const cat = new Pet('') {
-    name: Jasper,
-    age: 
+//this is a constructor function
+function Pet(name) {
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
+};
+//prototype properties in an object are the items seperated by commas within this.
+Pet.prototype = {
+  changeName(Pet) {
+    this.name = name;
+  },
+  growUp(Pet) {
+    this.age += 1;
+    this.hunger * 5;
+    this.fitness -= 3;
+  }
 }
 
-function Pet(name) {
-    this.name = name;
-  }
-
-new Pet('Jasper');*/
+//creating a new pet
+const cat = new Pet('Kitty')
 
 
-//constructor funtion
-function Pet (name) {
-    this.name = name
-  };
-  
-//object
-  pet.prototype = {
-    talk: function () {
-      console.log('WOOF!')
+/** a shared prototype modifies the instance it's called on. this below creates a walk method
+pet.prototype = {
+    walk (){
+        this.fitness +=5
     }
-  }
-  
+}
+
+//calling the unique pets info
+cat.fitness()
+//this will show you the fitness level of the pet **/
+
+//create a method of growup on the prototype
 module.exports = Pet
